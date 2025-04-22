a656f1af6661f94b53082f13c5e7625dee4e0bf9
ajinkya-cell <ajinkyaadharkar@gmail.com>
2025-04-22 15:22:44 +0530

    Adding project

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import fs from 'fs'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    https: false
+  }
+  
+})
